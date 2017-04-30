d1505e0542502296e5c6e40328e759cf390a66f9
SparkCode <iseb@outlook.com>
2017-04-30 15:05:43 +0400

    rewritten with promises

--- js/site.js
+++ js/site.js
@@ -1,47 +1,39 @@
-var twichers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "agravein", "storbeck"];
+var twichers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "agravein", "tellMeWHY?"];
 
 $(document).ready(getData);
 
 function getData() {
     twichers.map(function (name) {
-        getTwitcherData(name, updatePage)
+        getTwitcherData(name)
+            .then((data) => ({
+                    profilePicture: data["logo"],
+                    status: data["status"],
+                    url: data["url"]}),
+                () => ({
+                    profilePicture: "https://www.twitch.tv/images/xarth/dead_glitch.png",
+                    status: "A streamer has closed their Twitch account (or the account never existed)",
+                    url: "#"}))
+            .then((result) => updatePage(result, name))
+            .then(() => getTwitcherStreamData(name))
+            .then((result) => updateStreamPageData(result, name))
+            .catch((error) => console.log(error));
     })
 }
 
 function updatePage(data, userName) {
-
-    var userName = userName;
-
-    if (data.hasOwnProperty("error"))
-    {
-        var profilePicture = "https://www.twitch.tv/images/xarth/dead_glitch.png";
-        var status = "A streamer has closed their Twitch account (or the account never existed)";
-        var url = "#";
-    }
-    else
-    {
-        profilePicture = data["logo"];
-        status = data["status"];
-        url = data["url"];
-    }
-
-    var element = $("div[hidden] .media").clone().first();
-    element.find(".profile-picture").attr("src", profilePicture);
+    let element = $("div[hidden] .media").clone().first();
+    element.find(".profile-picture").attr("src", data.profilePicture);
     element.find(".user-name").html(userName);
-    element.find(".status").html(status);
-    element.find(".url").attr("href", url);
+    element.find(".status").html(data.status);
+    element.find(".url").attr("href", data.url);
     element.attr("id", userName);
 
     $("#all").append(element);
 
     $("#all-user-count").html(twichers.length);
-
-    getTwitcherStreamData(userName, updateStreamPageData)
 }
 
 function updateStreamPageData(data, userName) {
-    if (data["stream"] == null) return;
-
     var status = data["stream"]["status"];
     var element = $("#" + userName);
     element.find(".status").html(status);
@@ -52,28 +44,34 @@ function updateStreamPageData(data, userName) {
     $("#online").append(element.clone());
 }
 
-
-
-
-function getTwitcherData(name, callback) {
-    $.ajax({
-        url:"https://wind-bow.gomix.me/twitch-api/channels/" + name,
-        success: function (data) {
-            callback(data, name);
-        },
-        dataType: "jsonp"
-    })
+function getTwitcherData(name) {
+    return new Promise((correct, reject) => {
+        $.ajax({
+            url:`https://wind-bow.gomix.me/twitch-api/channels/${name}`,
+            success: function (data) {
+                if (data.error)
+                    reject(data);
+                else
+                    correct(data);
+            },
+            dataType: "jsonp"
+        })
+    });
 }
 
-function getTwitcherStreamData(name, callback) {
-    $.ajax({
-        url: "https://wind-bow.gomix.me/twitch-api/streams/" + name,
-        success: function (data) {
-            callback(data, name);
-        },
-        dataType: "jsonp"
-    })
-    
+function getTwitcherStreamData(name) {
+    return new Promise((correct, reject) => {
+        $.ajax({
+            url: `https://wind-bow.gomix.me/twitch-api/streams/${name}`,
+            success: function (data) {
+                if (data["stream"])
+                    correct(data, name);
+                else
+                    reject();
+            },
+            dataType: "jsonp"
+        })
+    });
 }
 
 
