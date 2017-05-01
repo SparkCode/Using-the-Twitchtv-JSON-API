f73f2c69f9b1960477a7b2959856ca58e321e0be
SparkCode <iseb@outlook.com>
2017-05-01 19:50:16 +0400

    fixed a bug with generators

--- js/site.js
+++ js/site.js
@@ -1,28 +1,16 @@
 var twichers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "agravein", "tellMeWHY?"];
 
-$(document)
-    .ready(co(getData)
-        .catch(console.log));
+$(document).ready(getData);
 
-function* getData() {
-    for (let name of twichers)
-    {
-        yield* updatePage(name);
-    }
-
-    for (let name of twichers)
-    {
-        try {
-            yield* updateStreamPageData(name)
-        }
-        catch(e) {
-            console.log(e);
-        }
-    }
+function getData() {
+    twichers.map(name => {
+        co(updatePage(name)).catch(console.log);
+        co(updateStreamPageData(name)).catch(console.log);
+    });
 }
 
 function* updatePage(userName) {
-    var data = yield* getTwitcherData(userName);
+    var data = yield getTwitcherData(userName);
     var element = $("div[hidden] .media").clone().first();
     element.find(".profile-picture").attr("src", data.logo);
     element.find(".user-name").html(userName);
@@ -39,19 +27,19 @@ function* updateStreamPageData(userName) {
     var element = $("#" + userName);
     element.find(".status").html(status);
     element.find(".online").removeAttr("hidden");
-
     $("#online-user-count")[0].innerHTML++;
-
     $("#online").append(element.clone());
 }
 
-function* getTwitcherData(name) {
-    var dataFetch = yield fetch(`https://wind-bow.glitch.me/twitch-api/channels/${name}`);
-    var json = yield dataFetch.json();
-    return !json.error ? json
-        : {logo: "https://www.twitch.tv/images/xarth/dead_glitch.png",
-            status: "A streamer has closed their Twitch account (or the account never existed)",
-            url: "#"};
+function getTwitcherData(name) {
+    return fetch(`https://wind-bow.glitch.me/twitch-api/channels/${name}`)
+        .then(data => data.json())
+        .then(json => !json.error ? json
+                : {
+                    logo: "https://www.twitch.tv/images/xarth/dead_glitch.png",
+                    status: "A streamer has closed their Twitch account (or the account never existed)",
+                    url: "#"
+                });
 }
 
 function* getTwitcherStreamData(name) {
